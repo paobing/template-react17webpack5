9090feda67a3cacf8fcc4c50b1176ed85a3a8fd7
paobing <paobing_guo@163.com>
2021-10-21 17:41:52 +0800

    增加mock使用示例

--- resources/apis/mock.js
+++ resources/apis/mock.js
@@ -1,9 +1,18 @@
 import { mock } from "mockjs";
 
 mock('/user/info', 'get', {
-    name: "@cname",
-    ip: "@ip",
+    "cn_name": "@cname",
+    "time": "@datetime",
+    "now": "@now",
+    "image": "@image('1600x900')",
+    "area": "@city",
+    "ip": "@ip",
+    "upper": "@upper(@name)",
     "lists|20": [
-        {"id|+1": 1, name: "@name", "age|10-60": 1}
+        {
+            "id|+1": 1,
+            "name": "@name",
+            "age|10-60": 1,
+        }
     ]
 })
